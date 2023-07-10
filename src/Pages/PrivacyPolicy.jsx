1e80875cb3d324a592795296ccb379785d0e51c4
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-07-10 18:50:19 +0530

    bugs fixed

--- src/Pages/PrivacyPolicy.jsx
+++ src/Pages/PrivacyPolicy.jsx
@@ -1,14 +1,15 @@
+import { Link } from 'react-router-dom'
 import logo from '../Assets/firstlookLogo.png'
 
 const PrivacyPolicy = () => {
     return (
         <div className='w-11/12 mx-auto h-fit'>
-            <div className='flex'>
+            <Link to='/' className='flex'>
                 <img src={logo} alt="" className='mt-12 h-16 w-16' />
                 <p className='mt-[3.7rem] font-semibold text-3xl ml-3'>
                     FIRSTLOOK
                 </p>
-            </div>
+            </Link>
 
             <p className="text-[24px] flex justify-center font-oxygen font-bold mt-16">
                 PRIVACY POLICY
