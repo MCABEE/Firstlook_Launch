86eb367042857b85ecfd5205105a12aa1db4a5b3
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-07-11 15:36:45 +0530

    page not found added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Index from "./Pages/Index"
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PrivacyPolicy from "./Pages/PrivacyPolicy";
+import PageNotFound from "./Pages/PageNotFound";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Index />} />
           <Route path='/privacypolicy' element={<PrivacyPolicy />} />
+          <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
